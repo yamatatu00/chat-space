6dec6602c9acfc05527633c72e2175bb0092c922
yamatatu00 <tattuxan@swan.ocn.ne.jp>
2019-07-06 18:30:49 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatSpace
+  class Application < Rails::Application
+    # Javascript・アプリケーションをテストするためのファイルが同時に生成されます。
+    # 今回作成するChatSpaceでは、必要のないファイルなので、以下追記して生成しないようにする
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
