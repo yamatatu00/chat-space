5efa9501f5c1db4e2c4d6d65c324dd4a4b2af4c1
yamatatu00 <tattuxan@swan.ocn.ne.jp>
2019-07-21 15:33:55 +0900

    自動更新修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -79,27 +79,18 @@ $(function(){
 
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data('id')
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log('success');
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       messages.forEach(function(message){
         insertHTML += buildMessageHTML(message);
       });
-      //メッセージが入ったHTMLを取得
-      //メッセージを追加
       $('.messages').append(insertHTML)
       scroll();
     })
